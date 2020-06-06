218866fe4d843726bd3e44b1d6791eae5e9f8f36
Eseosa Aigbogun <eseosa.aigbogun@conzultrix.com>
2020-06-06 17:31:38 +0100

    built Hero component

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+function Hero(props) {
+  return (
+    <main className="hero">
+      <h1>{props.header}</h1>
+      <p>{props.body}</p>
+    </main>
+  );
+}
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hero: {'title': '', 'body': ''},
+    };
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api')
+    .then(res => {
+      console.log(res.data);
+      this.setState({
+        hero: {'title': res.data[0].title, 'body': res.data[0].body},
+      })
+    });
+  }
   render() {
     return(
       <div>
-        <p>The Home Page</p>
+        <Hero 
+        header={this.state.hero.title} 
+        body={this.state.hero.body} 
+        />
       </div>
     );
   }
